569dc7e39c0d0cecf30c0ed92c6f21609c83b1ac
Francesco Fera <francescofera@gmail.com>
2023-09-02 16:57:43 +0200

    index disabled

--- scripts/search/prepare-index.js
+++ scripts/search/prepare-index.js
@@ -1,33 +1,33 @@
-import path from "path";
-import { promises as fs } from "fs";
-import { globby } from "globby";
-import grayMatter from "gray-matter";
+// import path from "path";
+// import { promises as fs } from "fs";
+// import { globby } from "globby";
+// import grayMatter from "gray-matter";
 
-(async function () {
-  // prepare the dirs
-  const mainDir = path.join(process.cwd(), "../..");
-  const srcDir = path.join(mainDir, "/src");
-  const publicDir = path.join(mainDir, "/public");
-  const contentDir = path.join(srcDir, "pages", "posts");
-  const contentFilePattern = path.join(contentDir, "*.md");
-  const indexFile = path.join(publicDir, "search-index.json");
-  const getSlugFromPathname = (pathname) =>
-    path.basename(pathname, path.extname(pathname));
-  //const contentFilePaths = await globby(curr);
-  console.log("Content" + contentDir);
+// (async function () {
+//   // prepare the dirs
+//   const mainDir = path.join(process.cwd(), "../..");
+//   const srcDir = path.join(mainDir, "/src");
+//   const publicDir = path.join(mainDir, "/public");
+//   const contentDir = path.join(srcDir, "pages", "posts");
+//   const contentFilePattern = path.join(contentDir, "*.md");
+//   const indexFile = path.join(publicDir, "search-index.json");
+//   const getSlugFromPathname = (pathname) =>
+//     path.basename(pathname, path.extname(pathname));
+//   //const contentFilePaths = await globby(curr);
+//   console.log("Content" + contentDir);
 
-  const contentFilePaths = await fs.readdir(contentDir, (err, files) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      return;
-    }
-    const mdFiles = files.filter((file) => path.extname(file) === ".md");
-    const filePaths = mdFiles.map((path) => path.join(contentDir, file));
+//   const contentFilePaths = await fs.readdir(contentDir, (err, files) => {
+//     if (err) {
+//       console.error("Error reading directory:", err);
+//       return;
+//     }
+//     const mdFiles = files.filter((file) => path.extname(file) === ".md");
+//     const filePaths = mdFiles.map((path) => path.join(contentDir, file));
 
-    console.log("MD Files:", filePaths);
-    return filePaths;
-  });
-  console.log(contentFilePaths);
+//     console.log("MD Files:", filePaths);
+//     return filePaths;
+//   });
+//   console.log(contentFilePaths);
 
   // if (contentFilePaths.length) {
   //   const files = contentFilePaths.map(
