ec61b5dd9d53e9cff4a3d14c38673d24ae0734c2
Francesco Fera <francescofera@gmail.com>
2023-09-03 11:10:01 +0200

    new index

--- scripts/search/prepare-index.js
+++ scripts/search/prepare-index.js
@@ -1,59 +1,86 @@
-// import path from "path";
-// import { promises as fs } from "fs";
-// import { globby } from "globby";
-// import grayMatter from "gray-matter";
+import path from "path";
+import { promises as fs } from "fs";
+import grayMatter from "gray-matter";
 
-// (async function () {
-//   // prepare the dirs
-//   const mainDir = path.join(process.cwd(), "../..");
-//   const srcDir = path.join(mainDir, "/src");
-//   const publicDir = path.join(mainDir, "/public");
-//   const contentDir = path.join(srcDir, "pages", "posts");
-//   const contentFilePattern = path.join(contentDir, "*.md");
-//   const indexFile = path.join(publicDir, "search-index.json");
-//   const getSlugFromPathname = (pathname) =>
-//     path.basename(pathname, path.extname(pathname));
-//   //const contentFilePaths = await globby(curr);
-//   console.log("Content" + contentDir);
+const mainDir = path.join(process.cwd(), "../..");
+const srcDir = path.join(mainDir, "/src");
+const publicDir = path.join(mainDir, "/public");
+const contentDir = path.join(srcDir, "pages", "posts");
+const indexFile = path.join(publicDir, "search-index.json");
+const getSlugFromPathname = (pathname) =>
+  path.basename(pathname, path.extname(pathname));
+let contentFilePaths;
 
-//   const contentFilePaths = await fs.readdir(contentDir, (err, files) => {
-//     if (err) {
-//       console.error("Error reading directory:", err);
-//       return;
-//     }
-//     const mdFiles = files.filter((file) => path.extname(file) === ".md");
-//     const filePaths = mdFiles.map((path) => path.join(contentDir, file));
+async function findMDFiles(directoryPath) {
+  try {
+    const files = await fs.readdir(directoryPath);
 
-//     console.log("MD Files:", filePaths);
-//     return filePaths;
-//   });
-//   console.log(contentFilePaths);
+    const fileExtension = ".md";
+    const mdFiles = files.filter(
+      (file) => path.extname(file) === fileExtension
+    );
 
-// if (contentFilePaths.length) {
-//   const files = contentFilePaths.map(
-//     async (filePath) => await fs.readFile(filePath, "utf8")
-//   );
-//   const index = [];
-//   let i = 0;
-//   for await (let file of files) {
-//     const {
-//       data: { title, description, tags },
-//       content,
-//     } = grayMatter(file);
-//     index.push({
-//       slug: getSlugFromPathname(contentFilePaths[i]),
-//       category: "blog",
-//       title,
-//       description,
-//       tags,
-//       body: content,
-//     });
-//     i++;
-//   }
-//   await fs.writeFile(indexFile, JSON.stringify(index));
-//   console.log(
-//     `Indexed ${index.length} documents from ${contentDir} to ${indexFile}`
-//   );
-//   console.log("finished");
-// }
-// })();
+    const filePaths = mdFiles.map((file) => path.join(directoryPath, file));
+
+    return filePaths;
+  } catch (err) {
+    console.error("Error reading directory:", err);
+    throw err;
+  }
+}
+
+async function processMDFiles(contentFilePaths, indexFile, contentDir) {
+  if (contentFilePaths.length) {
+    const files = contentFilePaths.map(async (filePath) => {
+      try {
+        const fileContent = await fs.readFile(filePath, "utf8");
+        return { filePath, fileContent };
+      } catch (error) {
+        console.error(`Error reading file ${filePath}:`, error);
+        return null;
+      }
+    });
+
+    const index = [];
+    for (const filePromise of files) {
+      const fileData = await filePromise;
+      if (fileData) {
+        const {
+          data: { title, description, tags },
+          content,
+        } = grayMatter(fileData.fileContent);
+        index.push({
+          slug: getSlugFromPathname(fileData.filePath),
+          category: "blog",
+          title,
+          description,
+          tags,
+          body: content,
+        });
+      }
+    }
+
+    try {
+      await fs.writeFile(indexFile, JSON.stringify(index));
+      console.log(
+        `Indexed ${index.length} documents from ${contentDir} to ${indexFile}`
+      );
+      console.log("Finished");
+    } catch (error) {
+      console.error("Error writing index file:", error);
+    }
+  }
+}
+
+(async function () {
+  findMDFiles(contentDir)
+    .then((filePaths) => {
+      contentFilePaths = filePaths;
+      processMDFiles(contentFilePaths, indexFile, contentDir);
+      console.log("contentFilePaths ", filePaths);
+      console.log("contentFilePaths LENGTH", contentFilePaths.length);
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+    });
+})();
